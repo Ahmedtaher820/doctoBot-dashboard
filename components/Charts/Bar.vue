75a90857c86f109b642aa4ebb2bca0cb026f83e3
Ahmedtaher820 <et8989784@gmail.com>
2023-06-26 05:01:58 +0300

    add charts in index

---
+++ components/Charts/Bar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <Bar :data="chartData" :options="chartOptions" />
+      
+    </div>
+  </template>
+  <script setup>
+  import { Bar, Line  } from 'vue-chartjs';
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+  } from 'chart.js';
+  import { ref } from 'vue';
+  ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+  );
+  
+  const chartData = ref({
+    labels: ['January', 'February', 'March', 'April', 'May','Jun','Jul'],
+    datasets: [
+      {
+        label: 'Number of reservations in month',
+        backgroundColor: '#77CEFF',
+        data: [40, 20, 12, 50, 10,25,20],
+      },
+    ],
+  });
+  const chartOptions = ref({
+    responsive: true,
+    maintainAspectRatio: false,
+  });
+  </script>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>
-      welcome
+    <div class="grid grid-cols-10 gap-8">
+      <div class="bg-white col-span-6 p-6 rounded-lg">
+        <ChartsBar  />
+      </div>
+      <div class="col-span-4 bg-white p-6 rounded-lg">
+        <ChartsDoughnut />
+      </div>
     </div>
   </template>
   
   <script lang="ts" setup>
+
+
   </script>
   
   <style>
